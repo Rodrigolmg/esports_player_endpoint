dc338a96b00a0d96bc765a6b457c55578699436d
Rodrigolmg <rodrigo.leitemg@gmail.com>
2023-09-18 18:37:47 -0300

    Player detail use case test added

---
+++ lib/domain/repository/repository_contract.dart
@@ -0,0 +1,7 @@
+library repository_contract;
+
+import 'package:dartz/dartz.dart';
+import 'package:esports_player_endpoint/core/core.dart';
+import 'package:esports_player_endpoint/domain/entity/entity.dart';
+
+part 'player_detail_repository.dart';
