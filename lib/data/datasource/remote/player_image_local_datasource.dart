3c4137e85ed3acf5e53ef534c4065c03b0fc1828
Rodrigolmg <rodrigo.leitemg@gmail.com>
2023-09-19 13:53:13 -0300

    Player image remote datasource implemented

--- lib/data/datasource/remote/player_image_local_datasource.dart
+++ lib/data/datasource/remote/player_image_local_datasource.dart
@@ -3,3 +3,27 @@ part of datasource;
 abstract class PlayerImageDataSource {
   Future<Uint8List?> getPlayerImage(int? playerId);
 }
+
+class PlayerImageDataSourceImpl implements PlayerImageDataSource {
+
+  final DioMethod dio;
+
+  const PlayerImageDataSourceImpl({
+    required this.dio,
+  });
+
+  @override
+  Future<Uint8List?> getPlayerImage(int? playerId) async {
+    Response response = await dio.getMethod(UrlPath.playerImagePath(playerId));
+    if(response.statusCode != null && response.statusCode == 200){
+      if((response.data as String).isEmpty){
+        throw ServerException(statusCode: 204);
+      }
+
+      Uint8List bytes = ImageHandler.convertBase64(response.data);
+      return Future.value(bytes);
+    } else {
+      throw ServerException(statusCode: response.statusCode);
+    }
+  }
+}
